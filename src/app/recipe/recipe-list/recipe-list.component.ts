d40d4884d570887747d121209a6d32a2cb85c5ce
grada92 <danielegradassai@gmail.com>
2023-11-10 19:29:49 +0100

    Edit Recipe-Detail-Component

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[]= [
+    new Recipe('a test recipe', 'test2','https://media-assets.lacucinaitaliana.it/photos/6318b459e70ad59cdf9c9266/4:3/w_1000,h_750,c_limit/montanarine1.jpg'),
+    new Recipe('a test recipe', 'test2','https://media-assets.lacucinaitaliana.it/photos/6318b459e70ad59cdf9c9266/4:3/w_1000,h_750,c_limit/montanarine1.jpg')
+
+  ];
+
+  constructor(){}
+
+  ngOnInit(){}
 
 }
