d3fe8f1c1028478bc03903ad05ad4cd7d4718c28
grada92 <danielegradassai@gmail.com>
2023-11-13 11:05:39 +0100

    add recipecomponent and recipedetail

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit{
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[]= [
     new Recipe('a test recipe', 'test2','https://media-assets.lacucinaitaliana.it/photos/6318b459e70ad59cdf9c9266/4:3/w_1000,h_750,c_limit/montanarine1.jpg'),
     new Recipe('a test recipe', 'test2','https://media-assets.lacucinaitaliana.it/photos/6318b459e70ad59cdf9c9266/4:3/w_1000,h_750,c_limit/montanarine1.jpg')
@@ -16,5 +17,9 @@ export class RecipeListComponent implements OnInit{
   constructor(){}
 
   ngOnInit(){}
+
+  OnRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 
 }
